0b71a7adf7729b664313d97094e4cf19f8338cf2
Huikai <huikai.chen@rea-group.com>
2018-11-05 19:59:26 +0800

    Update package.json

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ const path = require('path');
 
 app.use("/", expressStaticGzip(path.resolve(__dirname, "../build"), {}));
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(3000);
