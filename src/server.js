ae3f9bc76036b4fcf53ff6f7a7a748fedee1692a
Huikai <huikai.chen@rea-group.com>
2018-11-07 22:58:34 +0800

    Add fallback route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+const fallback = require('express-history-api-fallback')
 const express = require("express");
 const expressStaticGzip = require("express-static-gzip");
 const app = express();
 const path = require('path');
 
 app.use("/", expressStaticGzip(path.resolve(__dirname, "../build"), {}));
-
+app.use(fallback(path.resolve(__dirname, "../build/index.html")));
 app.listen(process.env.PORT || 3000);
